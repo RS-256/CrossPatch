76d5154169d0750b055875d3a833b855aa1262f9
RS256 <143985949+RS-256@users.noreply.github.com>
2026-07-16 16:32:50 +0900

    build: fuck the RCE#2

--- build.obfuscated.gradle.kts
+++ build.obfuscated.gradle.kts
@@ -82,7 +82,10 @@ dependencies {
     modImplementation("com.github.sakura-ryoko:malilib:${sc.current.version}-${property("deps.malilib")}")
     modImplementation("com.github.sakura-ryoko:litematica:${sc.current.version}-${property("deps.litematica")}")
     modImplementation("com.github.sakura-ryoko:itemscroller:${sc.current.version}-${property("deps.itemscroller")}")
-    modImplementation("me.fallenbreath:tweakermore:${property("deps.tweakermore")}-mc${sc.current.version}")
+    modImplementation("me.fallenbreath:tweakermore:${property("deps.tweakermore")}-mc${sc.current.version}") {
+        // temporary, newer version should set newer dependency, check tweakermore
+        exclude(group = "fi.dy.masa.litematica")
+    }
     modImplementation("maven.modrinth:flashback:${property("deps.flashback")}")
     modImplementation("maven.modrinth:bobby:${property("deps.bobby")}")
     modImplementation("com.github.bawnorton.mixinsquared:mixinsquared-fabric:${property("deps.mixinsquared")}")
